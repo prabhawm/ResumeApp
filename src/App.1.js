114d78f83f0832e4524d856e5159fec2a89a9fef
prabhawm <prabhawm@gmail.com>
2019-02-13 17:44:35 +0800

    added resume

--- src/App.1.js
+++ src/App.1.js
@@ -10,16 +10,22 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
         
-         
+          //<img src={logo} className="App-logo" alt="logo" />
           
           <p>
              {/* Edit <code>src/App.js</code> and save to reload. */}             
               
-           
-            <iframe title='document' src={resume} width='1366px' height='623px' frameborder='0' className="CV"> 
-            </iframe>
+            This is fresh.. 
+            
           </p>
-         
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
           
         </header>
       </div>
