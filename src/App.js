4cb0757db57265da7d5bf38580edd40d6d19b0aa
prabhawm <prabhawm@gmail.com>
2019-02-13 18:14:17 +0800

    title changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import resume from './PVP_DevOps_L2.pdf'
 import './App.css';
 
@@ -8,15 +7,10 @@ class App extends Component {
     return (
       
       <div className="App">
-        <header className="App-header">
-        
-         
+        <header className="App-header">          
           
           <p>
-             {/* Edit <code>src/App.js</code> and save to reload. */}             
-              
-           
-            <iframe title='document' src={resume} width='1366px' height='623px' frameborder='0' className="CV"> 
+            <iframe title='document' src={resume} width='1366px' height='623px' frameBorder='0' className="CV"> 
             </iframe>
           </p>
          
